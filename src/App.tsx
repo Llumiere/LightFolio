7e4b63708526bd75d9c91f3031db3966c5147f2a
Light Adegunle <lumieroyaume@gmail.com>
2024-07-25 01:12:32 +0100

    Created a State management to monitor the Theme of the Webpage and displayed the NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/NavBar';
 
 
 function App() {
+  const [theme, setTheme] = useState(0);
   return (
     < >
-
+      <Navbar heading="Java" textColor={theme === 0 ? "dark":'light' } themeColor={theme === 0 ? "light":'dark'} onSelect={() => null}></Navbar>
     </>
   );
 }
